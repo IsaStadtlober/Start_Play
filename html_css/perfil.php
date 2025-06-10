94a70e97b0209837df0cdb91f14b83b362f4fceb
Madu Almeida <dudacsantos450@gmail.com>
2025-06-10 20:19:09 -0300

    Update perfil.php

--- html_css/perfil.php
+++ html_css/perfil.php
@@ -24,14 +24,15 @@
     <title>Meu Perfil - StartPlay</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="formulario.css">
 </head>
-<body class="bg-light min-vh-100 d-flex align-items-center justify-content-center">
+<body id="cadastro" class="bg-light min-vh-100 d-flex align-items-center justify-content-center">
     <div class="card shadow px-3 py-4" style="max-width: 450px; width: 100%;">
         <div class="text-center mb-4">
-            <div class="rounded-circle bg-dark bg-opacity-25 d-flex align-items-center justify-content-center mx-auto mb-2" style="width: 100px; height: 100px; font-size: 4rem;">
+            <div class="rounded-circle bg-opacity-25 d-flex align-items-center justify-content-center mx-auto mb-2" style="width: 100px; height: 100px; font-size: 5rem;">
                 <i class="bi bi-person-circle text-dark"></i>
             </div>
-            <h2 class="fw-bold mb-3">Meu Perfil</h2>
+            <h2 class="fw-bold mb-1">Meu Perfil</h2>
         </div>
         <div class="mb-4 ps-2" style="margin-left:auto; margin-right:auto; max-width: 95%; text-align: left;">
             <p class="mb-2"><span class="fw-semibold text-secondary">Nome:</span> <?php echo htmlspecialchars($usuario['nomecompleto']); ?></p>
@@ -41,7 +42,7 @@
             <p class="mb-2"><span class="fw-semibold text-secondary">Sexo:</span> <?php echo htmlspecialchars($usuario['sexo']); ?></p>
             <p class="mb-2"><span class="fw-semibold text-secondary">Nome da Mãe:</span> <?php echo htmlspecialchars($usuario['nomematerno']); ?></p>
             <p class="mb-2"><span class="fw-semibold text-secondary">CPF:</span> <?php echo htmlspecialchars($usuario['cpf']); ?></p>
-            <p class="mb-2"><span class="fw-semibold text-secondary">Celular:</span> <?php echo htmlspecialchars($usuario['telefonecelular']); ?></p>
+            <p class="mb-2"><span class="fw-semibold text-secondary">Telefone Celular:</span> <?php echo htmlspecialchars($usuario['telefonecelular']); ?></p>
             <p class="mb-2"><span class="fw-semibold text-secondary">Telefone Fixo:</span> <?php echo htmlspecialchars($usuario['telefonefixo']); ?></p>
         </div>
         <div class="d-flex justify-content-center gap-2 flex-wrap mt-2">
