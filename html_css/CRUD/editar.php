a1e864ccfca3de077c3cb01912db972cdc8a71fd
Madu Almeida <dudacsantos450@gmail.com>
2025-06-10 20:53:51 -0300

    Update editar.php

--- html_css/CRUD/editar.php
+++ html_css/CRUD/editar.php
@@ -129,11 +129,17 @@
                     <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($usuario['email']); ?>" required>
                 </div>
                 <div class="d-flex justify-content-between">
-                    <button type="submit" class="btn btn-success">Salvar Alterações</button>
+                    <button type="submit" class="btn btn-success mt-3 d-flex align-items-center gap-2">
+                        <i class="bi bi-save"></i> Salvar Alterações
+                    </button>
                     <?php if($origem == 'crud'): ?>
-                        <a href="consulta.php" class="btn btn-secondary">Cancelar</a>
+                        <a href="consulta.php" class="btn btn-secondary mt-3 d-flex align-items-center gap-2">
+                            <i class="bi bi-x-circle"></i> Cancelar
+                        </a>
                     <?php else: ?>
-                        <a href="../perfil.php" class="btn btn-secondary">Cancelar</a>
+                        <a href="../perfil.php" class="btn btn-secondary mt-3 d-flex align-items-center gap-2">
+                            <i class="bi bi-x-circle"></i> Cancelar
+                        </a>
                     <?php endif; ?>
                 </div>
             </form>
