34a4fe9a744c0e17c1075ffa215b9d6e80d4c502
Isabelle Stadtlober <isabellestadtlober@gmail.com>
2025-06-21 15:43:31 -0300

    atualização do bd

--- html_css/banco_de_dados_startplay/BD_Startplay/SQL_startplay/bd_startplay.sql
+++ html_css/banco_de_dados_startplay/BD_Startplay/SQL_startplay/bd_startplay.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `startplay` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
-USE `startplay`;
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: startplay
+-- Host: localhost    Database: startplay
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.4.32-MariaDB
 
@@ -114,14 +112,6 @@ LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'startplay'
---
-
---
--- Dumping routines for database 'startplay'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -132,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-20 21:40:51
+-- Dump completed on 2025-06-21 15:37:23
