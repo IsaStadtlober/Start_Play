e11cb17faa595d2d45c93cc6e08c66cccb7f0cdc
Marcus Vinicius <marcuskn337@gmail.com>
2025-09-09 14:49:24 -0300

    ainda há erros mas amnh ajeito

--- html_css/banco_de_dados_startplay/conexao/conexao.php
+++ html_css/banco_de_dados_startplay/conexao/conexao.php
@@ -7,16 +7,8 @@
     $conn = mysqli_connect($host, $user, $password, $database);
     // Verifica se a conexão foi bem-sucedida
     if ($conn){
-       /*echo "Conexão bem-sucedida!";*/
+        /*echo "Conexão bem-sucedida!";*/
     } else {
         echo "Erro ao conectar ao banco de dados: " . mysqli_connect_error();
     }
-
-    /*Função para data ser exibida no formato brasileiro no CRUD
-    function formatodata($data){
-        $data = explode('-', $data);
-        $data = $data[2].'/'.$data[1].'/'.$data[0];
-        return $data;
-        // Exemplo: 2023-10-01 será exibido como 01/10/2023
-    }*/
 ?>
