b87e2325fb83fda86724e45e78faeadec306e03a
Beatriz <132065986+santosbtrz@users.noreply.github.com>
2025-10-10 20:02:05 -0300

    Update nao_log.php

--- html_css/nao_log.php
+++ html_css/nao_log.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <title>Não Logado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light d-flex align-items-center justify-content-center" style="min-height: 100vh;">
 
-    <div class="p-4 text-center">
-        <div class="d-flex align-items-center justify-content-center mt-3 mb-3">
-            <img src="img/logo-pp2.png" alt="Logo" class="me-2" style="max-width: 40px;">
-            <h1 class="fs-4 mb-0">StartPlay</h1>
+<body class="bg-light py-4">
+    <main class="d-flex align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5">
+                    <div class="p-3 text-center">
+                        <div class="d-flex align-items-center justify-content-center mt-2 mb-2">
+                            <img src="img/logo-pp2.png" alt="Logo" class="me-2" style="max-width: 40px;">
+                            <h1 class="fs-4 mb-0">StartPlay</h1>
+                        </div>
+                        <h2 class="mt-3 mb-2 text-danger fs-2">ACESSO NEGADO</h2>
+                        <p class="mb-0">Para acessar esta página, é necessário estar logado em sua conta.</p>
+                        <p class="mb-4">Retorne para o home e faça seu cadastro para continuar utilizando o site.</p>
+                        <a href="index.php" class="btn btn-primary w-100">Voltar</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h2 class="mb-3 mt-4 text-danger fs-2">ACESSO NEGADO</h2>
-        <p class="mb-4">Para acessar esta página, é necessário estar logado em sua conta.<br> Retorne para o home e faça seu cadastro para continuar utilizando o site.</p>
-        <a href="index.php" class="btn btn-primary w-50">Voltar</a>
-    </div>
+    </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
