18f59d1f2398a9ee5517bc85e27bbe377b6f344a
Madu Almeida <dudacsantos450@gmail.com>
2025-10-16 11:01:29 -0300

    Update página de nao-logado

--- html_css/nao_log.php
+++ html_css/nao_log.php
@@ -7,6 +7,7 @@
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <title>Não Logado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dark_mode.css">
 </head>
 
 <body class="bg-light py-4">
@@ -16,13 +17,13 @@
                 <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5">
                     <div class="p-3 text-center">
                         <div class="d-flex align-items-center justify-content-center mt-2 mb-2">
-                            <img src="img/logo-pp2.png" alt="Logo" class="me-2" style="max-width: 40px;">
+                            <img src="img/logo-pp2.png" alt="Logo" class="logo-startplay me-2" style="max-width: 40px;">
                             <h1 class="fs-4 mb-0">StartPlay</h1>
                         </div>
                         <h2 class="mt-3 mb-2 text-danger fs-2">ACESSO NEGADO</h2>
                         <p class="mb-0">Para acessar esta página, é necessário estar logado em sua conta.</p>
                         <p class="mb-4">Retorne para o home e faça seu cadastro para continuar utilizando o site.</p>
-                        <a href="index.php" class="btn btn-primary w-100">Voltar</a>
+                        <a href="index.php" class="btn btn-outline-primary w-100">Voltar</a>
                     </div>
                 </div>
             </div>
@@ -30,6 +31,7 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="dark_mode.js"></script>
 </body>
 
 </html>
